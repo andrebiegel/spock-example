70cdf8d5de8b4a0d5ea1e2d60418069ec604c771
André Biegel <andre.biegel@unymira.com>
2019-12-20 13:21:25 +0100

    basic example

--- src/test/groovy/GebishOrgSpec.groovy
+++ src/test/groovy/GebishOrgSpec.groovy
@@ -5,6 +5,12 @@ import org.testcontainers.containers.BrowserWebDriverContainer
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.Network
 import spock.lang.Shared
+import spock.lang.Title
+import spock.lang.Subject
+import spock.lang.Narrative
+import spock.lang.Issue
+@Title("Accessing Geb Documentation")
+@Narrative("To visit the Geb Documentation the following steps have to be done. ")
 class GebishOrgSpec extends GebReportingSpec {
 
     @Shared
@@ -23,20 +29,24 @@ class GebishOrgSpec extends GebReportingSpec {
         baseUrl = "http://gebish.org"
     }
 
+    @Issue("http://my.issues.org/FOO-1")
     def "can get to the current Book of Geb"() {
-        when:
+        when: "Firstly visit the Geb Homepage"
         to GebishOrgHomePage
+        report 'HomePage'
 
-        and:
+        and: "Open the Menue in the right upper corner"
         manualsMenu.open()
+        report 'Open Menu'
 
-        then:
+        then: "Scroll to the First Link of the Menu"
         manualsMenu.links[0].text().startsWith("current")
 
-        when:
+        when: "Click the menu item"
         manualsMenu.links[0].click()
 
-        then:
+        then: "You will automatically be shon the book of Geb"
         at TheBookOfGebPage
+        report 'book of Geb'
     }
 }
---
+++ src/test/resources/GebConfig.groovy
@@ -0,0 +1,5 @@
+    import com.aoe.gebspockreports.GebReportingListener
+    reportingListener = new GebReportingListener()
+    reportsDir = 'target/geb-spock-reports'
+    reportOnTestFailureOnly = false
+
